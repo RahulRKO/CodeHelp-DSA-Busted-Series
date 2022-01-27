e29de918828386dbc5fc417bf16941f2eab0ba2c
RahulRKO <94542741+RahulRKO@users.noreply.github.com>
2022-01-27 21:17:02 +0530

    Create lecture39.cpp

---
+++ lecture39.cpp
@@ -0,0 +1,38 @@
+//permutations of string
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    private:
+    
+    void solve(string S, int index,vector<string>&ans){
+        
+        //base case jab index S string par kre
+        if(index>=S.length()){
+            ans.push_back(S);
+            return;
+        }
+        
+        for(int i=index;i<S.length();i++){
+            swap(S[index],S[i]);
+            solve(S,index+1,ans);
+            //backtrack wapis original string banne keliye
+            swap(S[index],S[i]);
+        }
+    }
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    vector<string> ans;
+		    int index=0;
+		    solve(S,index,ans);
+		    sort(ans.begin(),ans.end());
+		    return ans;
+		}
+};
+
+
+
